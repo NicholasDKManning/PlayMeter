fa5960df601fd918a3e375081f6a4a5e2358be29
Nicholas Manning <nicholas.d.k.manning@gmail.com>
2025-07-04 02:58:28 -0500

    Setup everything to begin coding front end for day 2

---
+++ playmeter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    // Parental UI Home Page
+
+    <div className="min-h-screen bg-gray-100 text-gray-800 p-4">
+      <title>Hi, Nicholas</title>
+      <h1 className="text-2x1 font-bold">Hi, Nicholas</h1>      
+    </div>
+  )
+}
+
+export default App
